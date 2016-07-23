e378352dd06846ed2097872fe116f4de19f2a067
kwan3217 <kwan3217@gmail.com>
2016-07-23 11:44:33 -0600

    Added NMEAPlayback

--- Simulator.cpp
+++ Simulator.cpp
@@ -130,6 +130,22 @@ void Simulator::testNMEA() {
 	}
 }
 
+double NMEAPlayback::checkPPS(){
+  return t;
+}
+bool NMEAPlayback::checkNavChar(){
+  bool result=feof(inf)==0;
+  return result;
+}
+char NMEAPlayback::readChar(){
+  char c;
+  fread(&c,1,1,inf);
+  return c;
+}
+double NMEAPlayback::time(){
+  return t;
+}
+
 //++++++++++++++++++++++++++++++++++++++++++++++++++Servo methods.
 
 SimServo::SimServo(int cmdmin, int cmdmax, double physmin, double physmax, double slewrate) :
