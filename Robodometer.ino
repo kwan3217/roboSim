8ad29341049c2f76f0251677f9b49807b36f8b4c
Chris Jeppesen (Omoikane) <omoikane.kwansystems.org>
2016-08-10 22:41:58 +0000

    Starting to incorporate wire slave

--- Robodometer.ino
+++ Robodometer.ino
@@ -1,22 +1,31 @@
-//Robodometer. See theory documentation at https://github.com/kwan3217/Yukari4Python/wiki/Odometry
+//Robodometer. See theory documentation at https://github.com/kwan3217/roboSim/wiki/Odometry
+#include <Wire.h>
+
+
+extern "C" {
+  #include "utility/twi.h"
+}
 
 struct State {
-  /* 0x00      */ int32_t  WC __attribute((packed));
-  /* 0x04      */ uint32_t DT __attribute((packed));
-  /* 0x08      */ uint32_t T0 __attribute((packed));
-  /* 0x0C      */ uint32_t T1 __attribute((packed));
-  /* 0x10      */ uint16_t ID __attribute((packed));
-  /* 0x12      */ uint16_t RST __attribute((packed));
-  /* 0x14,0x16 */ uint16_t A[2] __attribute((packed));
-  /* 0x18,0x1A */ uint16_t A_min[2] __attribute((packed));
-  /* 0x1C,0x1E */ uint16_t A_max[2] __attribute((packed));
-  /* 0x20,0x22 */ uint16_t A_low[2] __attribute((packed));
-  /* 0x24,0x26 */ uint16_t A_high[2] __attribute((packed));
-  /* 0x28,0x29 */ uint8_t  A_lowfrac[2] __attribute((packed));
-  /* 0x2A,0x2B */ uint8_t  A_highfrac[2] __attribute((packed));
-  /* 0x2C,0x2E */ uint16_t servo_high[2] __attribute((packed));
-  /* 0x30,0x32 */ uint16_t servo_total[2] __attribute((packed));
+  /* 0x00      */  int32_t WC;
+  /* 0x04      */ uint32_t DT;
+  /* 0x08      */ uint32_t T0;
+  /* 0x0C      */ uint32_t T1;
+  /* 0x10      */ uint16_t ID;
+  /* 0x12      */ uint16_t RST;
+  /* 0x14,0x16 */ uint16_t A[2];
+  /* 0x18,0x1A */ uint16_t A_min[2];
+  /* 0x1C,0x1E */ uint16_t A_max[2];
+  /* 0x20,0x22 */ uint16_t A_low[2];
+  /* 0x24,0x26 */ uint16_t A_high[2];
+  /* 0x28,0x29 */ uint8_t  A_lowfrac[2];
+  /* 0x2A,0x2B */ uint8_t  A_highfrac[2];
+  /* 0x2C,0x2E */ uint16_t servo_high[2];
+  /* 0x30,0x32 */ uint16_t servo_total[2];
+  /* 0x34      */ uint8_t  serial;
 };
+
+int regPtr=0;
 
 static union {
   char regs[sizeof(State)];
@@ -43,7 +52,7 @@ void setup() {
   pinMode(A1,OUTPUT);
   pinMode(A2,OUTPUT);
   pinMode(A3,OUTPUT);
-  pinMode(A4,INPUT);
+//  pinMode(A4,INPUT);
   pinMode(13,OUTPUT);
   digitalWrite(A2,LOW);
   digitalWrite(A1,HIGH);
@@ -62,15 +71,52 @@ void setup() {
     state.servo_total[i]=2000;
     initServo(i);
   }
-  Serial.begin(38400);
+  Serial.begin(115200);
   Serial.println("Tstart,A0,A0min,A0max,A0low,A0high,black0,A1,A1min,A1max,A1low,A1high,black1,sector");
+  Wire.onReceive(receiveEvent); // register event
+  Wire.onRequest(requestEvent); // register event
+  Wire.begin(0x55);                // join i2c bus with address #8
 }
 
 void readSensors() {
   state.T0=micros();
   state.A[0]=analogRead(0);
-  state.A[1]=analogRead(4);
+//  state.A[1]=analogRead(4);
   state.T1=micros();
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+  regPtr=Wire.read();
+  Serial.print(regPtr,HEX);
+  while (Wire.available()>0) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+    regs[regPtr]=c;    
+    if(regPtr==0x34) {
+      Serial.print(regs[regPtr]);         // print the character
+    } else {
+      regPtr++;
+      if(regPtr==0x0C) regPtr=0;
+    }
+  }
+}
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() {
+  for(int i=0;i<4;i++) {  
+    Wire.write(regs[regPtr]);     
+    Serial.print(regs[regPtr],0);
+    twi_reply(1);
+    //move the reg ptr
+    if(regPtr==0x34) {
+    
+    } else {
+      regPtr++;
+      if(regPtr==0x0C) regPtr=0;
+    }
+  }
 }
 
 void figureThreshold(int i) {
@@ -133,8 +179,8 @@ void printState() {
 }
 
 void loop() {
-  readSensors();
-  figure();
-  printState();
+//  readSensors();
+//  figure();
+//  printState();
 }
 
