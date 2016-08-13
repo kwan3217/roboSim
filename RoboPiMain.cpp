0c5217bc5f35375896b83efd4c4956403aee4585
kwan3217 <kwan3217@gmail.com>
2016-08-13 16:40:00 +0000

    Code for RoboPiZ2.bin

--- RoboPiMain.cpp
+++ RoboPiMain.cpp
@@ -10,13 +10,16 @@ char servoChannel[] {'T','S','T','S','S','T','T'};
 int servoCommand[]  {150,150,140,200,150,150,150};
 OpenLoopGuidance guide(interface,t,servoChannel,servoCommand);
 
+int ouf;
+
 void setup() {
+  ouf=fileno(stdout);
 //  int whoami=interface.mpu.whoami();
 //  printf("whoami: %02x",whoami);
 //  while(guide.time()<15) {
-  printf("t,ax,ay,az,gx,gy,gz,T\n");
-
 }
+
+char buf[128];
 
 void loop() {
 //	interface.update(0.05);
@@ -26,7 +29,12 @@ void loop() {
 //	std::cout << std::endl;
   int16_t gx,gy,gz;
   interface.mpu.readGyro(gx,gy,gz);
-  printf("%0.4f,%d,%d,%d\n",interface.time(),gx,gy,gz);
+  writeBuf_le(buf,0,uint32_t(interface.time()*1'000'000));
+  writeBuf_le(buf,4,gx);
+  writeBuf_le(buf,6,gy);
+  writeBuf_le(buf,8,gz);
+  write(ouf,buf,10);
+  usleep(1000);
 }
 
 int main() {
