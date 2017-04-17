862ea6001b19bc5e9e85f0b04d0b42ce9c48a073
Chris Jeppesen (Omoikane) <omoikane.kwansystems.org>
2017-04-17 19:06:39 +0000

    Starting to reorganize code

---
+++ interface/HardwarePi.cpp
@@ -0,0 +1,186 @@
+#include <time.h>
+#include "HardwarePi.h"
+#include <wiringPi.h>
+#include <fcntl.h>
+#include <termios.h>
+
+/**
+ * @copydoc Servo::write(int)
+ * \internal
+ * Implemented by writing to the correct registers in the Arduino serving as the odometer. The value written is a 16-bit unsigned integer
+ * written little-endian to address 0x2C/0x2D or 0x2E/0x2F. This number is interpreted as the pulse width to use in 10us units. There is a software stop at 100 units and 200 units.
+ */
+void HardwarePiServoArduino::write(int n) {
+  if(n<100) n=100;
+  if(n>200) n=200;
+  writeI2Creg_le<uint16_t>(bus,ADDRESS,0x2C+2*channel,n);
+}
+
+/**
+ * @copydoc Interface::checkPPS()
+ * \internal
+ * Implemented using the LinuxPPS driver, which in turn implements RFC2783, Pulse-Per-Second API. If the interface epoch isn't valid
+ * at the first PPS, then the epoch is set to the time of the PPS.
+ *
+ */
+bool HardwarePiInterface::checkPPS(double& t) {
+  pps_info_t info;
+  static const struct timespec timeout={0,0};
+  time_pps_fetch(pps,PPS_TSFMT_TSPEC,&info,&timeout);
+  t=ts2t(dt(info.assert_timestamp));
+  bool has_new=(last_pps.tv_sec!=info.assert_timestamp.tv_sec) || (last_pps.tv_nsec!=info.assert_timestamp.tv_nsec);
+  if(has_new) last_pps=info.assert_timestamp;
+
+//	return 0; //Comment out PPS stuff because no PPS source is plugged in
+}
+
+/** Makes sure that GPS buffer has data to read. If there is still data in the buffer that hasn't been spooled out, return immediately. If
+ * we have read to the end of the buffer, then try to read a full buffer, but set the length of data left to the amount we actually read.
+ * The file has been opened with O_NONBLOCK so it should return immediately even if there isn't a full buffer worth of data to read.
+ */
+void HardwarePiInterface::fillGpsBuf() {
+  if(gpsPtr<gpsLen) return;
+  gpsLen=fread(gpsBuf,1,sizeof(gpsBuf),gpsf);
+  gpsPtr=0;
+}
+
+/**
+ * @copydoc Interface::checkNavChar()
+ * \internal
+ * Implemented with an internal buffer. First, the buffer is filled if necessary with fillGpsBuf. Then, return true if there is more than 1 byte in the buffer.
+ */
+bool HardwarePiInterface::checkNavChar() {
+  fillGpsBuf(); //Make sure the buffer has data in it
+  return gpsLen!=0;
+}
+
+/**
+ * @copydoc Interface::readChar()
+ * \internal
+ * Implemented with an internal buffer. First, the buffer is filled if necessary with
+ * fillGpsBuf. Then, return the character pointed to by the buffer read pointer, and
+ * increment that pointer. If checkNavChar would return false, this will read the
+ * character at index 0 in the buffer, which is old data.
+ */
+char HardwarePiInterface::readChar() {
+  fillGpsBuf();
+  char result=gpsBuf[gpsPtr];
+  gpsPtr++;
+  return result;
+}
+
+/**
+ * @copydoc Interface::time()
+ * \internal
+ * Implemented by reading the system clock, then subtracting off the epoch of the first time the clock was read.
+ * Keeps timestamps and integers as long as possible so as not to lose precision.
+ */
+double HardwarePiInterface::time() {
+  struct timespec ts;
+  clock_gettime(CLOCK_REALTIME,&ts);
+  return ts2t(dt(ts));
+}
+
+/** @copydoc Interface::button(int)
+ *  \internal
+ *  Since we use WiringPiSetupGpio(), we use the Broadcom numbers. This happens to match the numbers printed
+ *  on our Pi header.
+ */
+bool HardwarePiInterface::button(int pin) {
+  return 0==digitalRead(pin);
+}
+
+void HardwarePiInterface::readOdometer(uint32_t &timeStamp, int32_t &wheelCount, uint32_t &dt) {
+  ioctl(bus,I2C_SLAVE,ODOMETER_ADDRESS);
+  char buf[0x0C];
+  buf[0]=0x00;
+  write(bus,buf,1);
+  read(bus,buf,12);
+  wheelCount=readBuf_le<int32_t>(buf,0);
+  dt        =readBuf_le<uint32_t>(buf,4);
+  timeStamp =readBuf_le<uint32_t>(buf,8);
+}
+
+bool HardwarePiInterface::readAcc(int16_t a[]) {
+  return mpu.readGyro(a[0],a[1],a[2]);
+}
+
+bool HardwarePiInterface::readAcc(int16_t a[], int16_t& t) {
+  return mpu.readGyro(a[0],a[1],a[2],t);
+}
+
+bool HardwarePiInterface::readGyro(int16_t g[], int16_t& t) {
+  return mpu.readGyro(g[0],g[1],g[2],t);
+}
+
+bool HardwarePiInterface::readGyro(int16_t g[]) {
+  return mpu.readGyro(g[0],g[1],g[2]);
+}
+
+bool HardwarePiInterface::readMag(int16_t b[]) {
+  bool result=mpu.ak.read(b[1],b[0],b[2]);
+  b[2]=-b[2];
+  return result;
+}
+
+bool HardwarePiInterface::readMPU(int16_t a[], int16_t g[], int16_t& t) {
+  return mpu.readMPU(a[0],a[1],a[2],g[0],g[1],g[2],t);
+}
+
+HardwarePiInterface::HardwarePiInterface(Servo& Lsteering, Servo& Lthrottle):Interface(Lsteering,Lthrottle) {
+  //Set epoch
+  clock_gettime(CLOCK_REALTIME,&t0);
+  //Setup for GPIO (for buttons)
+  wiringPiSetupGpio();
+
+  //Open the GPS serial port
+  int igps =open("/dev/ttyAMA0",O_NONBLOCK| O_RDONLY);
+
+  /* set the other settings (in this case, 9600 8N1) */
+  struct termios settings;
+  tcgetattr(igps, &settings);
+
+  cfsetospeed(&settings, B9600); /* baud rate */
+  cfsetispeed(&settings, B9600); /* baud rate */
+  settings.c_cflag &= ~PARENB; /* no parity */
+  settings.c_cflag &= ~CSTOPB; /* 1 stop bit */
+  settings.c_cflag &= ~CSIZE;
+  settings.c_cflag |= CS8 | CLOCAL; /* 8 bits */
+  settings.c_lflag = ICANON; /* canonical mode */
+  settings.c_oflag &= ~OPOST; /* raw output */
+
+  tcsetattr(igps, TCSANOW, &settings); /* apply the settings */
+  tcflush(igps, TCOFLUSH);
+
+  gpsf=fdopen(igps,"r"); //Get a FILE* from the int file descriptor
+  setbuf(gpsf,nullptr);  //Turn off buffering
+
+  //Open PPS source
+  ppsf=fopen("/dev/pps0","r");
+  time_pps_create(fileno(ppsf), &pps);
+
+  //Open the I2C bus
+  bus=open("/dev/i2c-1",O_RDWR);
+  if(bus<0) printf("Couldn't open bus: errno %d",errno);
+
+  //Initialize the MPU9250
+  mpu.begin(bus);
+}
+
+HardwarePiInterface::~HardwarePiInterface() {
+  time_pps_destroy(pps);
+  fclose(ppsf);
+  close(bus);
+}
+
+HardwarePiInterfaceArduino::HardwarePiInterfaceArduino():
+HardwarePiInterface(hardSteering, hardThrottle), hardSteering(0),hardThrottle(1) {
+  hardSteering.begin(bus);
+  hardThrottle.begin(bus);
+};
+
+HardwarePiInterfaceArduino::~HardwarePiInterfaceArduino() {
+
+}
+
+
---
+++ interface/HardwarePi.h
@@ -0,0 +1,101 @@
+#ifndef HardwarePi_h
+#define HardwarePi_h
+/** @file */
+
+#include "robot.h"
+#include <sys/timepps.h>
+#include <cstdio> //used for FILE*
+#include "buffer.h"
+#include "I2C.h"
+#include "MPU.h"
+
+class HardwarePiServo: public Servo {
+public:
+  virtual void begin(I2C_t)=0;
+};
+
+/** Physical servo interface, via the Arduino which is also hosting the odometer
+ *
+ */
+class HardwarePiServoArduino: public HardwarePiServo {
+private:
+  I2C_t bus; ///<I2C bus stream, opened somewhere else because other devices are on the bus
+  static const int ADDRESS=0x55; ///< 7-bit address of Arduino
+  int channel; ///<Channel number to write, either 0 or 1
+public:
+  virtual void write(int n);
+  virtual void begin(I2C_t Lbus) {bus=Lbus;};
+  HardwarePiServoArduino(int Lchannel):channel(Lchannel),bus(-1) {};
+  HardwarePiServoArduino(I2C_t Lbus, int Lchannel):HardwarePiServoArduino(Lchannel) {begin(Lbus);};
+  ~HardwarePiServoArduino() {};
+};
+
+/** Hardware interface using a Raspberry Pi, as currently intended for Yukari 4
+ *
+ */
+class HardwarePiInterface: public Interface {
+private:
+  struct timespec t0,last_pps; ///<Epoch time in Unix seconds, so we don't have to deal with an epoch over a billion seconds ago
+  FILE* ppsf; ///< PPS stream
+  pps_handle_t pps; ///<PPS instance handle
+  /** Convert a struct timespec into a count of seconds. The integer part of the return will be specified by tv_sec, while the fractional
+   * part is specified by tv_nsec.
+   * @param ts a struct timespec to convert
+   * @return equivalent timestamp in double precision count of seconds.
+   */
+  static double ts2t(struct timespec ts) {return ts.tv_sec+double(ts.tv_nsec)/1'000'000'000.0;};
+  /** Figure the distance from the epoch to a given timespec using pure integer math
+   @param ts given timespec
+   @return a deltat timespec, where tv_sec and tv_nsec represent the difference between the timestamps
+  */
+  struct timespec dt(struct timespec ts) {
+    struct timespec result;
+    result.tv_sec=ts.tv_sec-t0.tv_sec;
+    if(ts.tv_nsec<t0.tv_nsec) {
+      result.tv_sec++;
+      result.tv_nsec=(ts.tv_nsec+1'000'000'000)-t0.tv_nsec;
+    } else {
+      result.tv_nsec=ts.tv_nsec-t0.tv_nsec;
+    }
+    return result;
+  }
+  static const int ODOMETER_ADDRESS=0x55; ///< 7-bit I2C address of Arduino used as odometer
+  char gpsBuf[128]; ///< GPS data buffer
+  FILE* gpsf; ///<GPS NMEA stream
+  int gpsLen; ///<Number of bytes in the GPS buffer
+  int gpsPtr; ///<Index of next byte to be read
+  void fillGpsBuf();
+protected:
+public:
+  I2C_t bus; ///< I2C bus stream
+  MPU9250 mpu;
+  virtual bool checkPPS(double& t);
+  virtual bool checkNavChar();
+  virtual char readChar();
+  virtual double time();
+  struct timespec epoch() {return t0;};
+  virtual void readOdometer(uint32_t &timeStamp, int32_t &wheelCount, uint32_t &dt);
+  virtual bool readAcc(int16_t a[]);
+  virtual bool readAcc(int16_t a[], int16_t& t);
+  virtual bool readMag(int16_t b[]);
+  virtual bool readGyro(int16_t g[]);
+  virtual bool readGyro(int16_t g[], int16_t& t);
+  virtual bool readMPU(int16_t a[], int16_t g[], int16_t& t);
+  virtual bool button(int pin=17);
+  HardwarePiInterface(Servo& steering, Servo& throttle);
+  virtual ~HardwarePiInterface();
+};
+
+/** Hardware interface for Raspberry Pi, using the Arduino servo interface
+ *
+ */
+class HardwarePiInterfaceArduino: public HardwarePiInterface {
+private:
+  HardwarePiServoArduino hardSteering; ///< Steering servo interface
+  HardwarePiServoArduino hardThrottle; ///< Throttle servo interface
+public:
+  HardwarePiInterfaceArduino();
+  virtual ~HardwarePiInterfaceArduino();
+};
+
+#endif
