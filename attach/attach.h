c3602a673a1bae13e9f0acece40b9715b555466d
kwan3217 <kwan3217@gmail.com>
2017-04-18 15:01:22 +0000

    Committing for pull

---
+++ attach/attach.h
@@ -0,0 +1,11 @@
+#ifndef dump_h
+#define dump_h
+
+#include "log/Log.h"
+
+extern char _binary_attach_tbz_start[];
+extern char _binary_attach_tbz_end[];
+
+void dumpAttach(Log& pkt, int apid, int pktSize=64);
+
+#endif
