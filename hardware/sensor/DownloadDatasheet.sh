bccd106e05e817c0b765487317354150f4ca9f96
kwan3217 <kwan3217@gmail.com>
2017-04-19 15:52:00 +0000

    Moved datasheets and pcb files closer to their driver code

---
+++ hardware/sensor/DownloadDatasheet.sh
@@ -0,0 +1,4 @@
+wget https://www.invensense.com/wp-content/uploads/2015/02/PS-MPU-9250A-01-v1.1.pdf       #MPU9250 9DoF sensor datasheet
+wget https://www.invensense.com/wp-content/uploads/2015/02/RM-MPU-9250A-00-v1.6.pdf       #Register map
+wget https://www.akm.com/akm/en/file/datasheet/AK8963C.pdf                                #AK8963 magnetometer, embedded in MPU9250
+
